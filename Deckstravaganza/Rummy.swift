0278904e0c58948d093c284bb313fcfa34791b6d
Calvin Moore <c1moore@ufl.edu>
2015-09-28 12:08:52 -0400

    Rummy is now conforming to necessary protocols.

--- Deckstravaganza/Rummy.swift
+++ Deckstravaganza/Rummy.swift
@@ -8,12 +8,54 @@
 
 import Foundation
 
+struct RummyMeld {
+    var user: Player;
+    var meld: StackPile;
+}
+
 class Rummy: CardGame {
     var deck = Deck();
     
     var wastePile =     StackPile(),
-        foundations =   [StackPile](),
-        tableus =       [StackPile]();
+        playersHands =  [StackPile](),
+        melds =         [RummyMeld]();
     
+    var players = [Player]();
     
+    func play() {
+        print("Playing");
+    }
 }
+
+class RummyDelegate: CardGameDelegate {
+    typealias T = Rummy;
+    
+    func deal(Game: T) {
+        print("Dealt");
+    }
+    
+    func gameDidStart(Game: T) {
+        print("Game started");
+    }
+    
+    func gameDidEnd(Game: T) {
+        print("Game ended");
+    }
+    
+    func isWinner(Game: T) {
+        print("Winner!");
+    }
+    
+    func roundDidStart(Game: T) {
+        print("Round started");
+    }
+    
+    func roundDidEnd(Game: T) {
+        print("Round ended");
+    }
+    
+    func increaseScore(Game: T) {
+        print("Score increased");
+    }
+}
+

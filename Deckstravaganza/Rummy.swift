db09a2d4156a97304b9dd223cfe502373d5f4c0c
Calvin Moore <c1moore@ufl.edu>
2015-09-28 11:44:08 -0400

    Began working on Rummy protocol

---
+++ Deckstravaganza/Rummy.swift
@@ -0,0 +1,19 @@
+//
+//  Rummy.swift
+//  Deckstravaganza
+//
+//  Created by Cory Armstrong on 9/28/15.
+//  Copyright © 2015 University of Florida. All rights reserved.
+//
+
+import Foundation
+
+class Rummy: CardGame {
+    var deck = Deck();
+    
+    var wastePile =     StackPile(),
+        foundations =   [StackPile](),
+        tableus =       [StackPile]();
+    
+    
+}

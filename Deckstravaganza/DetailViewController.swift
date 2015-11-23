75dddf9c7465c11b200af3bfe2a288851843b4ed
Calvin Moore <c1moore@ufl.edu>
2015-11-23 14:25:41 -0500

    Fixed conflicts.

--- Deckstravaganza/DetailViewController.swift
+++ Deckstravaganza/DetailViewController.swift
@@ -40,54 +40,6 @@ class DetailViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-  
-    var menu: Menu!{
-        didSet (newMenu) {
-            if(menu.name == "Test")
-           {
-                self.hideAllUI()
-                self.testGameUI()
-            }
-            if(menu.name == "New")
-            {
-               self.newGameUI()
-            }
-            if(menu.name == "Continue")
-            {
-                self.hideAllUI()
-                self.continueGameUI()
-            }
-            if(menu.name == "Saved Games")
-            {
-                self.hideAllUI()
-                self.savedGameUI()
-            }
-        }
-    }
-    
-    var Description = UILabel(frame: CGRectMake(0,0,200,21))
-    let btntwo = UIButton(type: UIButtonType.System)
-    let multlabel = UILabel(frame: CGRectMake(0,0,200,21))
-    let switch1 = UISwitch(frame:CGRectMake(150, 300, 0, 0))
-    
-    func testGameUI(){
-        
-        btntwo.hidden = false
-        Description.hidden = false
-        
-        Description.text = menu.description
-        Description.center = CGPointMake(120, 160)
-        Description.textAlignment = NSTextAlignment.Center
-        self.view.addSubview(Description)
-        
-        btntwo.setTitle(menu.name, forState: .Normal)
-        btntwo.backgroundColor = UIColor.whiteColor()
-        btntwo.frame = CGRectMake(160, 150, 100, 50)
-        btntwo.center = CGPointMake(350, 160 )
-        btntwo.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
-        self.view.addSubview(btntwo)
-    
-    }
     
     func tearDownMenuUI() {
         for element in formFields {
@@ -216,11 +168,87 @@ class DetailViewController: UIViewController {
 //    
 //    }
     
-    func buttonAction(sender:UIButton!)
-    {
-        print("Button tapped")
-        performSegueWithIdentifier("menuToGameSegue", sender: nil)
-    }
+//    func newGameUI(){
+//        
+//        multlabel.hidden = false
+//        switch1.hidden = false
+//        
+//        multlabel.center = CGPointMake(100, 100)
+//        multlabel.textAlignment = NSTextAlignment.Center
+//        multlabel.text = "Multiplayer"
+//        self.view.addSubview(multlabel)
+//        
+//        switch1.on = true
+//        switch1.setOn(true, animated: false)
+//        switch1.addTarget(self, action: "switchValueDidChange:", forControlEvents: .ValueChanged)
+//        switch1.center = CGPointMake(200, 100)
+//        self.view.addSubview(switch1)
+//        
+//        Description.text = "Game Settings"
+//        Description.center = CGPointMake(75, 75)
+//        Description.textAlignment = NSTextAlignment.Center
+//        self.view.addSubview(Description)
+//        
+//        btntwo.setTitle("Play", forState: .Normal)
+//        btntwo.frame = CGRectMake(200, 200, 100, 50)
+//        btntwo.center = CGPointMake(200, 300)
+//        btntwo.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+//        self.view.addSubview(btntwo)    }
+//    
+//    func continueGameUI(){
+//        
+//        btntwo.hidden = false
+//        Description.hidden = false
+//        
+//        Description.text = menu.description
+//        Description.center = CGPointMake(120, 160)
+//        Description.textAlignment = NSTextAlignment.Center
+//        self.view.addSubview(Description)
+//        
+//        btntwo.setTitle(menu.name, forState: .Normal)
+//        btntwo.center = CGPointMake(350, 165)
+//        btntwo.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+//        self.view.addSubview(btntwo)
+//     
+//    }
+//    
+//    func savedGameUI(){
+//        btntwo.hidden = false
+//        Description.hidden = false
+//        
+//        Description.text = menu.description
+//        Description.center = CGPointMake(120, 160)
+//        Description.textAlignment = NSTextAlignment.Center
+//        self.view.addSubview(Description)
+//        
+//        btntwo.setTitle(menu.name, forState: .Normal)
+//        btntwo.center = CGPointMake(350, 165)
+//        btntwo.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+//        self.view.addSubview(btntwo)
+//    }
+//    
+//    func hideAllUI(){
+//        btntwo.hidden = true
+//        Description.hidden = true
+//        multlabel.hidden = true
+//        switch1.hidden = true
+//    }
+//    
+//    func switchValueDidChange(sender:UISwitch!)
+//    {
+//        if (sender.on == true){
+//            print("on")
+//        }
+//        else{
+//            print("off")
+//        }
+//    }
+//    
+//    func testAction(sender:UIButton!)
+//    {
+//        print("Button tapped")
+//        performSegueWithIdentifier("menuToGameSegue", sender: nil)
+//    }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if(segue.identifier != nil && segue.identifier == "menuToGameSegue") {

1381e35e885a82e46b34360c9424cdcdf1a1fb7f
ltcarbonell <lt.carbonell@gmail.com>
2015-11-23 14:38:26 -0500

    fix rummy issues

--- Deckstravaganza/GameSceneViewController.swift
+++ Deckstravaganza/GameSceneViewController.swift
@@ -9,44 +9,6 @@
 import SpriteKit
 
 class GameSceneViewController: UIViewController {
-//    var gameScene: SKScene!
-//    var game: Rummy!
-//    //var gameDelegate: RummyDelegate!
-//    var gameDelegate: SolitaireDelegate!
-//    
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        // Do any additional setup after loading the view.
-//        let spriteView: SKView = self.view as! SKView
-//        spriteView.showsDrawCount = true
-//        spriteView.showsNodeCount = true
-//        spriteView.showsFPS = true
-//    }
-//    
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//    
-//    override func viewWillAppear(animated: Bool) {
-//        //solitaireScene = SolitaireScene(size: CGSizeMake(768, 1024))
-//        let gameName = "Solitaire"
-//        //        if gameName == "Solitaire" {
-//        //            self.game = Solitaire()
-//        //            self.gameDelegate = SolitaireDelegate()
-//        //            self.gameScene = SolitaireScene(gameScene: self, size: CGSizeMake(768, 1024))
-//        //        } else if gameName == "Rummy" {
-//        self.game = Rummy(numberOfPlayers: 2)
-//        //self.gameDelegate = RummyDelegate()
-//        self.gameScene = RummyScene(gameScene: self, game: self.game, /*gameDelegate: self.gameDelegate,*/ size: CGSizeMake(768, 1024))
-//        //        }
-//        let spriteView:SKView = self.view as! SKView
-//        spriteView.presentScene(gameScene)
-//        
-//    }
-    
     var gameType: GameType!;
     
     override func viewDidLoad() {
@@ -81,8 +43,8 @@ class GameSceneViewController: UIViewController {
         
         let spriteView:SKView = self.view as! SKView
         spriteView.presentScene(gameScene)
-    }
-
+    
+    
     /*
     // MARK: - Navigation
     
@@ -93,4 +55,6 @@ class GameSceneViewController: UIViewController {
     }
     */
     
+    }
 }
+
